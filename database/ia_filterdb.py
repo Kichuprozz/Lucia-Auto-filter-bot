5743ae8adf22ddf5d1dc996f6e611ac870525079
Kichuprozz <128233036+Kichuprozz@users.noreply.github.com>
2025-08-01 12:25:17 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -76,7 +76,7 @@ async def check_db_size(db):
     
 async def save_file(media):
     file_id, file_ref = unpack_new_file_id(media.file_id)
-    file_name = re.sub(r"@\w+|(_|\-|\.|\+|\#|\$|%|\^|&|\*|\(|\)|!|~|`|,|;|:|\"|\'|\?|/|<|>|\[|\]|\{|\}|=|\||\\)", " ", str(media.file_name))
+    file_name = re.sub(r"@\w+|(_|\-|\.|\+|\#|\$|%|\^|&|\*|\(|\)|!|~|`|,|;|:|\"|\?|/|<|>|\[|\]|\{|\}|=|\||\\)", " ", str(media.file_name))
     file_name = re.sub(r"\s+", " ", file_name)    
     saveMedia = Media
     if MULTIPLE_DB:
